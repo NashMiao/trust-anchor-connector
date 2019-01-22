09129f1af1c45e4d95c5d053e7148aa1dc7012f9
dingxin <wdx7266@vip.qq.com>
2019-01-22 23:57:50 +0800

    update

--- connector/chesicc.py
+++ connector/chesicc.py
@@ -17,37 +17,40 @@ class ChesiccMethod(object):
         return f'https://www.chsi.com.cn{img_pos}'
 
 
-def query_edu(q_code: str):
-    url = ChesiccMethod.query_edu(q_code)
-    response = requests.post(url, timeout=10)
-    msg = response.content.decode('utf-8')
-    return msg
+class Chesicc(object):
+    @staticmethod
+    def query_edu(q_code: str):
+        if isinstance(q_code, int):
+            q_code = str(q_code)
+        url = ChesiccMethod.query_edu(q_code)
+        response = requests.post(url, timeout=10)
+        msg = response.content.decode('utf-8')
+        return msg
 
+    @staticmethod
+    def query_edu_img(img_pos: str):
+        url = ChesiccMethod.query_edu_img(img_pos)
+        img = Image.open(requests.get(url, stream=True).raw)
+        img.show()
 
-def query_edu_img(img_pos: str):
-    url = ChesiccMethod.query_edu_img(img_pos)
-    img = Image.open(requests.get(url, stream=True).raw)
-    img.show()
-
-
-def chsi_parser(page, get_img: bool = False):
-    soup = BeautifulSoup(page, features='lxml-xml')
-    result_table = soup.find('div', {'class': 'clearfix', 'id': 'resultTable'})
-    info_dict = dict()
-    if result_table is None:
+    @staticmethod
+    def chsi_parser(page, get_img: bool = False):
+        soup = BeautifulSoup(page, features='lxml-xml')
+        result_table = soup.find('div', {'class': 'clearfix', 'id': 'resultTable'})
+        info_dict = dict()
+        if result_table is None:
+            return info_dict
+        if get_img:
+            result_table.find('img', {'id': 'photoPositon'})
+        info_list = result_table.find_all('td')[3:33]
+        for index in range(0, len(info_list), 2):
+            info_key = info_list[index].contents[0]
+            info_value = info_list[index + 1].find('div').contents[0]
+            info_dict[info_key] = info_value.replace(' ', '').replace('\n', '')
         return info_dict
-    if get_img:
-        result_table.find('img', {'id': 'photoPositon'})
-    info_list = result_table.find_all('td')[3:33]
-    for index in range(0, len(info_list), 2):
-        info_key = info_list[index].contents[0]
-        info_value = info_list[index + 1].find('div').contents[0]
-        info_dict[info_key] = info_value.replace(' ', '').replace('\n', '')
-    return info_dict
 
-
-if __name__ == '__main__':
-    code = ''
-    content = query_edu(str(code))
-    info_dict = chsi_parser(content)
-    print(info_dict)
+    @staticmethod
+    def get_chsi_info(q_code: str):
+        content = Chesicc.query_edu(q_code)
+        info_dict = Chesicc.chsi_parser(content)
+        return info_dict
